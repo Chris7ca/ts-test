b4089540020c97232b14f4cb17a865e0358466f7
Christian Martínez <43973068+Chris7ca@users.noreply.github.com>
2023-02-01 12:16:36 -0600

    Add palindrome challenge

---
+++ palindrome.ts
@@ -0,0 +1,11 @@
+const PALINDROMES = ['Atar a la rata', 'Anita lava la tina', 'Roma ni se conoce sin oro, ni se conoce sin amor'];
+
+function isPalindrome(text: string): boolean {
+    // logic
+    return true;
+}
+
+for (const palindrome of PALINDROMES) {
+    const is = isPalindrome(palindrome);
+    console.log(`${palindrome} - ${is ? 'is palindrome' : 'is not palindrome'}`);
+}
